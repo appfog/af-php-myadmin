66159602d7e22c10a40f822ad32a33347af2b476
Tim Santeford <tsantef@gmail.com>
2012-11-20 01:12:12 -0800

    Added new auth type.

--- config.inc.php
+++ config.inc.php
@@ -19,42 +19,34 @@ $cfg['Error_Handler']['display'] = false;
  */
 
 /*
- * This is needed for cookie based authentication to encrypt password in
- * cookie
- */
-$cfg['blowfish_secret'] = 'afaff8b7c6d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
-
-/*
  * Servers configuration
  */
 $i = 0;
 
 $services_json = json_decode(getenv("VCAP_SERVICES"),true);
-foreach($services_json["mysql-5.1"] as $E)
-{
-$mysql_config = $E["credentials"];
 
-/*
- * First server
- */
-$i++;
-$g = $i-1;
-/* Authentication type */
-// Set 'auth_type' to config for cloud foundry and VCAP_SERVICES
-$cfg['Servers'][$i]['auth_type'] = 'config';
-/* Server parameters */
-$cfg['Servers'][$i]['host'] = $mysql_config["hostname"];
-$cfg['Servers'][$i]['verbose'] = $E["name"];
-$cfg['Servers'][$i]['connect_type'] = 'tcp';
-$cfg['Servers'][$i]['compress'] = false;
-/* Select mysql if your server does not have mysqli */
-$cfg['Servers'][$i]['extension'] = 'mysqli';
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
-$cfg['Servers'][$i]['user'] =  $mysql_config["username"];
-$cfg['Servers'][$i]['password'] =  $mysql_config["password"];
-$cfg['Servers'][$i]['only_db'] = $mysql_config["name"];
-$cfg['Servers'][$i]['hide_db'] = 'information_schema';
-$cfg['Servers'][$i]['ShowDatabasesCommand'] = 'SELECT DISTINCT TABLE_SCHEMA FROM information_schema.SCHEMA_PRIVILEGES';
+$cfg['Servers'][1]['auth_type'] = 'appfog';
+$cfg['bound_services'] = false;
+
+foreach($services_json["mysql-5.1"] as $E) {
+    $i++;
+
+    $mysql_config = $E["credentials"];
+
+    $cfg['Servers'][$i]['auth_type'] = 'appfog';
+    /* Server parameters */
+    $cfg['Servers'][$i]['host'] = $mysql_config["hostname"];
+    $cfg['Servers'][$i]['verbose'] = $E["name"];
+    $cfg['Servers'][$i]['connect_type'] = 'tcp';
+    $cfg['Servers'][$i]['compress'] = false;
+    /* Select mysql if your server does not have mysqli */
+    $cfg['Servers'][$i]['extension'] = 'mysqli';
+    $cfg['Servers'][$i]['AllowNoPassword'] = false;
+    $cfg['Servers'][$i]['user'] =  $mysql_config["username"];
+    $cfg['Servers'][$i]['password'] =  $mysql_config["password"];
+    $cfg['Servers'][$i]['only_db'] = $mysql_config["name"];
+    $cfg['Servers'][$i]['hide_db'] = 'information_schema';
+    $cfg['Servers'][$i]['ShowDatabasesCommand'] = 'SELECT DISTINCT TABLE_SCHEMA FROM information_schema.SCHEMA_PRIVILEGES';
+
+    $cfg['bound_services'] = true;
 }
-
-
